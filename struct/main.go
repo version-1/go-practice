dd89fdebfd7a60f2bd4678fd0852d9f42cab3ca1
version-1 <j.onodera26@gmail.com>
2021-03-11 18:38:06 -0800

    some fix

--- struct/main.go
+++ struct/main.go
@@ -8,32 +8,3 @@ func main () {
   fmt.Println("Hello")
 }
 
-type Array struct {
-  Data []int
-}
-
-func (a *Array) Add(value int) {
-  a.Data = append(a.Data, value)
-}
-
-func (a *Array) Remove(index int) {
-  a.Data = append(a.Data[:index], a.Data[index+1:]...)
-}
-
-func (a *Array) Find(value int) int {
-  for _, v := range a.Data {
-    if v == value {
-      return v
-    }
-  }
-  return -1
-}
-
-func (a *Array) FindIndex(value int) int {
-  for n, v := range a.Data {
-    if v == value {
-      return n
-    }
-  }
-  return -1
-}
